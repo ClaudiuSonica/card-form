1224f743870d25c724762fe23c3499c9700e9cb9
Claudiu Sonica <csonica95@gmail.com>
2023-05-11 21:22:02 +0200

    finished v1

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
-# card-form
+# Frontend Mentor - Interactive card details form solution
+
+This is a solution to the [Interactive card details form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-card-details-form-XpS8cKZDWw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Fill in the form and see the card details update in real-time
+- Receive error messages when the form is submitted if:
+  - Any input field is empty
+  - The card number, expiry date, or CVC fields are in the wrong format
+- View the optimal layout depending on their device's screen size
+- See hover, active, and focus states for interactive elements on the page
+
+## My process
+
+Firstly I construct the HTML structure, than i start styling it for mobile first, always, after i get the dimensions, colors, fonts and any other design aspect, i move on to the hover/focus/active states.
+After HTML and CSS is finished i start JS. 
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flex-box
+- CSS Grid
+- Mobile-first workflow
+
+
+### What I learned
+
+I learned about position absolute and relative, gradient colors and borders, more about html input forms and a lot about javascript syntax and logic. 
+
+```html
+<input 
+type="text"
+id="name"
+name="cardholder__name"
+placeholder="e.g Jane Appleseed"
+class="input"
+required
+/>
+```
+```css
+.input:hover,
+.input:focus {
+  cursor: pointer;
+  border: 2px solid transparent;
+  border-image: -webkit-gradient(
+    linear,
+    right top,
+    left top,
+    from(hsl(249, 99%, 64%)),
+    to(hsl(278, 94%, 30%))
+  );
+  border-image: linear-gradient(right, 
+  hsl(249, 99%, 64%), 
+  hsl(278, 94%, 30%)
+  );
+  border-image-slice: 1;
+}
+```
+```js
+function format(s) {
+  return s.toString().replace(/\d{4}(?=.)/g, "$&");
+}
+```
+
+### Continued development
+
+- More responsive layout
+- More interactive components
+- More animations
+- More interactions
+- More responsive typography
--- main.js
+++ main.js
@@ -1,44 +1,129 @@
-// Get elements from the user input 
-
 const form = document.querySelector("#form");
-const inputName = document.querySelector("#cardholder__name");
-const inputNumber = document.querySelector("#card__number");
-const inputCvv = document.querySelector("#cvv");
+
+const cardName = document.querySelector("#card__name");
+const inputName = document.querySelector("#name");
+
+const cardNumber = document.querySelector("#card__number");
+const inputNumber = document.querySelector("#number");
+
+const cardMonth = document.querySelector("#card__month");
 const inputMonth = document.querySelector("#month");
+
+const cardYear = document.querySelector("#card__year");
 const inputYear = document.querySelector("#year");
 
-// Get elements to output the result
+const cardCvv = document.querySelector("#card__cvv");
+const inputCvv = document.querySelector("#cvv");
 
-const outputName = document.querySelector("#name");
-const outputNumber = document.querySelector("#number");
-const outputCvv = document.querySelector("#cvv__card");
-const outputMonth = document.querySelector("#month__card");
-const outputYear = document.querySelector("#year__card");
+const completed = document.querySelector("#completed")
 
-const errorMessage = {
-  required: "Can't be blank",
-  invalid: "Wrong format, numbers only",
-  range: "Wrong format, respect length",
-};
-
-// Real time change from user input
-
-const validateForm = (e) => {
-  e.preventDefault();
-
-  const cardholderName = inputName.value;
-  const cardNumber = inputNumber.value;
-  const cvv = inputCvv.value;
-  const month = inputMonth.value;
-  const year = inputYear.value;
-
-  // Validate the name
-
-  
+const submitBtn = document.querySelector("#btn");
+const continueBtn = document.querySelector("#continue");
 
 
-
-
-
-
+function setCardName(e) {
+  cardName.innerText = e.target.value;
 }
+
+function setCardNumber(e) {
+  cardNumber.innerText = format(e.target.value);
+}
+
+function setCardMonth(e) {
+  cardMonth.innerText = e.target.value;
+}
+
+function setCardYear(e) {
+  cardYear.innerText = e.target.value;
+}
+
+function setCardCvv(e) {
+  cardCvv.innerText = e.target.value;
+}
+
+function format(s) {
+  return s.toString().replace(/\d{4}(?=.)/g, "$&");
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  
+  if (!inputName.value) {
+    inputName.classList.add("error__input");
+    inputName.parentElement.classList.add("error__message");
+  } else {
+    inputName.classList.remove("error__input");
+    inputName.parentElement.classList.remove("error__message");
+  }
+
+  if (!inputNumber.value) {
+    inputName.classList.add("error__input");
+    inputName.parentElement.classList.add("error__message");
+  } else if (inputNumber.value.length < 16) {
+    inputNumber.classList.add("error__input");
+  } else {
+    inputNumber.classList.remove("error__input");
+    inputNumber.parentElement.classList.remove("error__message");
+  }
+
+  if (!inputMonth.value) {
+    inputMonth.classList.add("error__input");
+    inputMonth.parentElement.classList.add("error__message");
+  } else {
+    inputMonth.classList.remove("error__input");
+    inputMonth.parentElement.classList.remove("error__message");
+  }
+
+  if (!inputYear.value) {
+    inputYear.classList.add("error__input");
+    inputYear.parentElement.classList.add("error__message");
+  } else {
+    inputYear.classList.remove("error__input");
+    inputYear.parentElement.classList.remove("error__message");
+  }
+
+  if (!inputCvv.value) {
+    inputCvv.classList.add("error__input");
+    inputCvv.parentElement.classList.add("error__message");
+  } else {
+    inputCvv.classList.remove("error__input");
+    inputCvv.parentElement.classList.remove("error__message");
+  }
+
+  if (
+    inputName.value &&
+    inputNumber.value &&
+    inputNumber.value.length == 16 &&
+    inputMonth.value &&
+    inputYear.value &&
+    inputCvv.value
+  ) {
+    completed.classList.remove("invisible");
+    form.classList.add("invisible");
+  }
+}
+
+function resetForm(e) {
+  e.preventDefault();
+  form.classList.remove("invisible");
+  completed.classList.add("invisible");
+  inputName.value = "";
+  inputNumber.value = "";
+  inputMonth.value = "";
+  inputYear.value = "";
+  inputCvv.value = "";
+  cardName.textContent = "Jane Appleseed";
+  cardNumber.textContent = "1234 5678 9123 0000";
+  cardMonth.textContent = "00";
+  cardYear.textContent = "00";
+  cardCvv.textContent = "000";
+}
+
+
+inputName.addEventListener("keyup", setCardName);
+inputNumber.addEventListener("keyup", setCardNumber);
+inputMonth.addEventListener("keyup", setCardMonth);
+inputYear.addEventListener("keyup", setCardYear);
+inputCvv.addEventListener("keyup", setCardCvv);
+submitBtn.addEventListener("click", handleSubmit);
+continueBtn.addEventListener("click", resetForm);
